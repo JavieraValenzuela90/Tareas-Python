bac0d9a3ef9d6776465ff00822112257f3950869
JavieraValenzuela90 <javiera.valenzuela.p@gmail.com>
2023-06-06 18:31:46 -0400

    Hola Mundo hecho

---
+++ Hola Mundo/Hola_mundo.py
@@ -0,0 +1,14 @@
+print("Hola Mundo")
+
+name = "Javi"
+print ("Hola", name )
+print ("Hola " + name)
+
+name = 13
+print ("Hola",name,"!")
+print ("Hola " + str(name) + "!")
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print (f"Amo comer {fave_food1} y {fave_food2}")
+print ("Amo comer {} y {}".format(fave_food1, fave_food2))
