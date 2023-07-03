79c3a94d40c102b7bbf0e3a56dc6955a2e48c777
JavieraValenzuela90 <javiera.valenzuela.p@gmail.com>
2023-07-02 22:18:25 -0400

    Ensayar en Hola Flask

--- Hola Flask/server.py
+++ Hola Flask/server.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", phrase="hola", times=5)# ¡Fíjate en los 2 nuevos argumentos nombrados!
 
+@app.route('/lists')
+def render_lists():
+    # Muy pronto, obtendremos datos de una base de datos, pero por ahora, estamos codificando datos
+    students = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = students)
+
+
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
 
