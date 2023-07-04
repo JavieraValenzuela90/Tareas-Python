ed7e87856a2666a2d6a5938d418b41ea65c546be
JavieraValenzuela90 <javiera.valenzuela.p@gmail.com>
2023-07-04 17:24:45 -0400

    Tarea Encuesta dojo hecha

--- Encuesta Dojo/server.py
+++ Encuesta Dojo/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, request
 
 app = Flask(__name__)
 
-app.secret_key="encuesta dojo"
+app.secret_key="esto es secreto"
 
 @app.route('/')
 def index():
@@ -15,11 +15,11 @@ def process():
     session['location'] = request.form['location']
     session['language'] = request.form['language']
     session['comments'] = request.form['comments']
-    return redirect('/success')
+    return redirect('/resultados')
 
-@app.route('/success')
-def success():
-    return render_template('success.html')
+@app.route('/resultados')
+def resultados():
+    return render_template('resultados.html')
     
 if __name__=="__main__":
     app.run(debug=True)
